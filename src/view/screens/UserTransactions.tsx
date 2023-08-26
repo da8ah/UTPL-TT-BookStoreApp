ac5b77fb503d809d0ea7f823b53ea577317ab06c
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-08-26 12:06:28 -0500

    fix: transactions with infinite scroll

--- src/view/screens/UserTransactions.tsx
+++ src/view/screens/UserTransactions.tsx
@@ -1,19 +1,38 @@
 import { useNavigation } from "@react-navigation/native"
 import { FlashList } from "@shopify/flash-list"
 import { Button, Icon, Text } from "@ui-kitten/components"
+import { useEffect, useState } from "react"
 import { View } from "react-native"
 import useClient from "../../hooks/useClient"
 import CardTransaction from "../../model/core/entities/CardTransaction"
 import { UserNavProps } from "../routes/types.nav"
 import { globalStyles as styles } from "../styles/styles"
 import TransactionCard from "./layouts/TransactionCard"
-import Cart from "../../model/core/entities/Cart"
-import ToBuyBook from "../../model/core/entities/ToBuyBook"
 
 export default function UserTransactions() {
     const navigation = useNavigation<UserNavProps>()
     const { client } = useClient()
-    const transactions = client.getTransactions().reverse() as CardTransaction[]
+    const [isLoading, setLoadingState] = useState(false)
+    const [transactions, setTransactions] = useState<CardTransaction[]>([])
+    const [counter, setCounter] = useState(1)
+
+    const allTransactions = [...client.getTransactions().reverse()] as CardTransaction[]
+    const loadTransactions = () => {
+        setLoadingState(true)
+        setTransactions(allTransactions.splice(0, counter * 10))
+        setTimeout(() => {
+            setLoadingState(false)
+        }, 200)
+
+        setCounter(counter => counter + 1)
+    }
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            loadTransactions()
+        }, 1000)
+
+        return () => { clearTimeout(timer) }
+    }, [])
 
     return <View style={[styles.common, styles.body]}>
         <View style={{ width: '100%', padding: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
@@ -27,15 +46,17 @@ export default function UserTransactions() {
                 <Text category="h2">Mis Compras</Text>
             </View>
         </View>
-        <View style={{ width: '100%', height: 500 }}>
+        <View style={{ flex: 1, width: '100%' }}>
             <FlashList
                 scrollEnabled
                 key={"transactions"}
                 contentContainerStyle={{ backgroundColor: "transparent" }}
                 estimatedItemSize={250}
+                refreshing={isLoading}
                 data={transactions}
-                extraData={transactions}
                 renderItem={TransactionCard}
+                onEndReachedThreshold={0.1}
+                onEndReached={loadTransactions}
             />
         </View>
     </View>
